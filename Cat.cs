3d49147c6a24abe26c708a7c488834a072993407
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-10-29 09:14:53 -0400

    Initial Commit

---
+++ Cat.cs
@@ -0,0 +1,49 @@
+﻿/************************************************
+ * A simple event handler demo
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * Reference: https://www.intertech.com/Blog/c-sharp-tutorial-understanding-c-events/
+ * **********************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpEventHandlerDemo {
+    /// <summary>
+    /// This class models a cat and the cat jumps onto the table. That causes an event
+    /// We need to know when that happens, hence the event.
+    /// </summary>
+    class Cat {
+        
+        // Set up a random number generator to simulate cat behavior. 
+        private static Random randomBehavior = new Random();
+        
+        /// <summary>
+        /// The cat name. Every cat should have a name
+        /// </summary>
+        public String name { get; set; }
+        
+        /// <summary>
+        /// A delegate that will receive the event notification
+        /// </summary>
+        public delegate void dgEventRaiserOnTable();
+
+        /// <summary>
+        /// The "Cat on Table" event. Delegates will listen for this event...
+        /// </summary>
+        public event dgEventRaiserOnTable OnTable;
+
+
+        /// <summary>
+        /// The Cat runs around like a maniac. Randomly he will jump on the table, causing an event to be raised.
+        /// </summary>
+        public void RunAround() {
+            Console.WriteLine(name + " is running around maniacally...");
+            if (randomBehavior.Next() % 3 == 0) {       // Every third episode he will jump on the table
+                this.OnTable();     // Raise the event. Someone will be listening!
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/************************************************
+ * A simple event handler demo
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * Reference: https://www.intertech.com/Blog/c-sharp-tutorial-understanding-c-events/
+ * **********************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +13,25 @@ using System.Threading.Tasks;
 namespace MyCSharpEventHandlerDemo {
     class Program {
         static void Main(string[] args) {
+            Cat siriusBlack = new Cat();
+
+            //***********************************************************************************
+            // Hook into the event: now we are listening. 
+            siriusBlack.OnTable += new Cat.dgEventRaiserOnTable(CatOnTableEventHandler);
+            //***********************************************************************************
+
+            siriusBlack.name = "Sirius Black";
+            // Run the cat around 10 times. Eventuallty it will jump on the table
+            for (int i = 0; i < 10; i++) {
+                //Console.WriteLine(siriusBlack.name + " is running around...");
+                siriusBlack.RunAround();
+            }
+        }
+        /// <summary>
+        /// This method will be called by the event handler in Cat. 
+        /// </summary>
+        static void CatOnTableEventHandler() {
+            Console.WriteLine("Red Alert: CAT IS ON TABLE");
         }
     }
 }
